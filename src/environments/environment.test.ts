fa35b685ebfcff3af314804ea33bb61a06f72540
corneil_jumpco <corneil@jumpco.io>
2020-09-24 16:19:08 +0200

    added artifacts for build to deploy

--- src/environments/environment.test.ts
+++ src/environments/environment.test.ts
@@ -9,7 +9,7 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: true,
+  production: false,
   baseHref: '/request-tracker-ui',
   baseAuthUrl: '/auth/realms/JBPM/protocol/openid-connect',
   baseUrl: '/kie-server/services/rest/server',
